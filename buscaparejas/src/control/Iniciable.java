016435b48a67274c780e4021a4f2fd0993bbbd3d
Juanantoniot10 <juanantoniot10hithub@gmail.com>
2018-03-20 22:41:47 +0100

    todo listo para empezar

--- buscaparejas/src/control/Iniciable.java
+++ buscaparejas/src/control/Iniciable.java
@@ -7,9 +7,11 @@ public interface Iniciable {
 	 * GenerarTablero(int fila, int columna),
 	 * RellenarTablero()crea una casilla por cada posicion del tablero y recibirá un número (referencia).
 	 * DesordenarCasillas() el tablero queda con las casillas desordenadas.,
-	 * 
+	 * @param ParaFilaYColumna Enum Dificultad que contiene un valor (.getValor)
+	 * que servira para las filas y las columnas
 	 */
-void iniciarJuego();
+
+void iniciarJuego(Dificultad ParaFilaYColumna);
 
 
 
--- buscaparejas/src/modelo/Casilla.java
+++ buscaparejas/src/modelo/Casilla.java
@@ -1,6 +1,26 @@
 package modelo;
 
 public class Casilla {
-private Boolean visible;
+private boolean visible = false;
 private int identificador;
+
+
+
+
+//TODO crear constructor que asigne identificador
+
+
+
+	public boolean isVisible() {
+		return visible;
+	}
+	public void setVisible(boolean visible) {
+		this.visible = visible;
+	}
+	public int getIdentificador() {
+		return identificador;
+	}
+	public void setIdentificador(int identificador) {
+		this.identificador = identificador;
+	}
 }
